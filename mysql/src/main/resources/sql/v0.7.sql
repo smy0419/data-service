32d4eabc94507b122d844b82346dc0f23b06860f
smy0419 <howdoyoudo0601@qq.com>
2020-06-24 15:05:44 +0800

    modify t_dao_message column

--- mysql/src/main/resources/sql/v0.7.sql
+++ mysql/src/main/resources/sql/v0.7.sql
@@ -1,3 +1,4 @@
 alter table t_dao_organization add column tx_hash varchar(64) NOT NULL COMMENT 'Transaction Hash';
 alter table `t_dao_organization` modify column `state` tinyint(2) COMMENT 'Organization Status: 0.normal 1.closed 2.local initialize';
 alter table `t_dao_message` add column height bigint(20) NOT NULL COMMENT 'Block Height';
+alter table `t_dao_message` modify column height bigint(20) DEFAULT 0 NOT NULL COMMENT 'Block Height';
